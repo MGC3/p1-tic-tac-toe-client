c8682ed5e775a72c4c739707d5173557854bce8f
michaelchun <michael.chun03@gmail.com>
2019-11-14 12:54:13 -0500

    Clicking on box now returns id of box clicked

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -18,9 +18,22 @@ const onGetAllGames = () => {
     .catch(ui.onGetAllGamesFailure);
 };
 
+const onClickSquare = e => {
+  // things to do: 
+  // check if move is valid, and exit/break if invalid
+  // render the X or O. Add a 'currentPlayerSymbol' boolean somewhere and toggle it.
+  // check for win condition
+  // send/PATCH api update with new move. If win, send that info too. Also if win, show user message
+  const id = e.target.id;
+  console.log(id);
+
+  api.
+};
+
 const addHandlers = e => {
   $('#create-game').on('click', onCreateGame);
   $('#get-all-games').on('click', onGetAllGames);
+  $('#game-board').on('click', onClickSquare);
 };
 
 module.exports = {
